62b2e4d3d2e7f8a77420bb98f12bc3ad4c6f3e48
Stas Merzliakov <merzliakov@gmail.com>
2013-11-12 12:04:20 +0400

    Добавил все модели FIAC

--- infosys/models.py
+++ infosys/models.py
@@ -281,3 +281,225 @@ class HouseInterval(Base):
 
 
 
+""" Описание мест расположения  имущественных объектов """
+class Landmark(Base):
+
+    __tablename__ = 'FIAC_Landmark'
+
+    """ Месторасположение ориентира """
+    location = Column(String(length=500), nullable=False)
+
+    """ Почтовый индекс """
+    postalcode = Column(String(length=6))
+
+    """ Код ИФНС ФЛ """
+    ifnsfl = Column(String(length=4))
+
+    """ Код территориального участка ИФНС ФЛ """
+    terrifsnfl = Column(String(length=4))
+   
+    """ Код ИФНС ЮЛ """
+    ifnsul = Column(String(length=4))
+
+    """ Код территориального участка ИФНС ЮЛ """
+    terrifnsul = Column(String(length=4))
+
+    """ ОКАТО """
+    okato = Column(String(length=11))
+
+    """ ОКТМО """
+    oktmo = Column(String(length=8))
+
+    """ Дата  внесения (обновления) записи """
+    updatedate = Column(Date, nullable=False)
+
+    """ Уникальный идентификатор записи ориентира """
+    """ HINT !!! """
+    landid = Column(String(length=36), primary_key=True)
+
+    """ Глобальный уникальный идентификатор ориентира """
+    """ HINT !!! """
+    landguid = Column(String(length=36), unique=True, nullable=False)
+
+    """ Идентификатор объекта родительского объекта (улицы, города, населенного пункта и т.п.) """
+    """ HINT !!! """
+    aoguid = Column(String(length=36), nullable=False)
+
+    """ Начало действия записи """
+    startdate = Column(Date, nullable=False)
+
+    """ Окончание действия записи """
+    enddate = Column(Date, nullable=False)
+     
+    """ Внешний ключ на нормативный документ """
+    """ HINT !!! """
+    normdoc = Column(String(length=36))
+
+
+""" Сведения по нормативному документу, являющемуся основанием присвоения адресному элементу наименования """
+class Normdoc(Base):
+
+    __tablename__ = 'FIAC_NormDoc'
+
+    """ Идентификатор нормативного документа """
+    normdocid = Column(String(length=36), primary_key=True)
+       
+    """ Наименование документа """
+    docname = Column(String)
+
+    """ Дата документа """
+    docdate = Column(Date)
+
+    """ Номер документа """
+    docnum = Column(String(length=20))
+
+    """ Тип документа """
+    """ HINT !!! """
+    doctype = Column(Integer,nullable=False)
+
+    """ Идентификатор образа (внешний ключ) """
+    """ HINT !!! """
+    docimgid = Column(Integer, nullable=False)
+
+
+
+""" Справочник. Тип адресного объекта """
+class AddressObjectType(Base):
+
+    __tablename__ = 'FIAC_AddressObjectType'
+ 
+    """ Уровень адресного объекта """
+    level = Column(Integer, nullable=False)
+    
+    """ Краткое наименование типа объекта """
+    scname = Column(String(length=10))
+ 
+    """ Полное наименовение типа объекта """
+    socrname = Column(String(length=50),nullable=False)
+ 
+    """ Ключевое поле """
+    kod_t_st = Column(String(length=4),primary_key=True)
+
+
+""" Статус актуальности КЛАДР 4.0 """
+class CurrentStatus(Base):
+
+    __tablename__ = 'FIAC_CurrentStatus'
+ 
+    """ Идентификатор статуса (ключ) """
+    curentstid = Column(Integer, primary_key=True)
+   
+    """ Наименование (0 - актуальный, 1-50, 2-98 – исторический (кроме 51), 51 - переподчиненный, 99 - несуществующий) """
+    name = Column(String(length=100), nullable=False)
+ 
+
+
+""" Статус актуальности ФИАС """
+class ActualStatus(Base):
+
+    __tablename__ = 'FIAC_ActualStatus'
+    
+    """ Идентификатор статуса (ключ)  """
+    actstatid  = Column(Integer, primary_key=True)
+ 
+    """ Наименование:
+         0 – Не актуальный
+         1 – Актуальный (последняя запись по адресному объекту) """
+    name = Column(String(length=100), nullable=False) 
+ 
+
+""" Статус действия """
+class OperationStatus(Base):
+   
+    __tablename__ = 'FIAC_OperationStatus'
+  
+    """ Идентификатор статуса (ключ) """
+    operstatid = Column(Integer, primary_key=True)
+
+    """ Наименование
+         01 – Инициация;
+         10 – Добавление;
+         20 – Изменение;
+         21 – Групповое изменение;
+         30 – Удаление;
+         31 - Удаление вследствие удаления вышестоящего объекта;
+         40 – Присоединение адресного объекта (слияние);
+         41 – Переподчинение вследствие слияния вышестоящего объекта;
+         42 - Прекращение существования вследствие присоединения к другому адресному объекту;
+         43 - Создание нового адресного объекта в результате слияния адресных объектов;
+         50 – Переподчинение;
+         51 – Переподчинение вследствие переподчинения вышестоящего объекта;
+         60 – Прекращение существования вследствие дробления;
+         61 – Создание нового адресного объекта в результате дробления;
+         70 – Восстановление объекта прекратившего существование"""
+    name = Column(String(length=100), nullable=False)
+  
+
+""" Статус центра """
+class CenterStatus(Base):
+   
+    __tablename__ = 'FIAC_CenterStatus'
+
+    """ Идентификатор статуса """
+    centerstid = Column(Integer, primary_key=True)
+
+    """ Наименование """
+    name = Column(String(length=100), nullable=False)
+
+
+""" Статус интервала домов """
+class IntervalStatus(Base):
+
+    __tablename__ = 'FIAC_IntervalStatus'
+
+    """ Идентификатор статуса """
+    intvstatid = Column(Integer, primary_key=True)
+
+    """ Наименование (обычный, четный, нечетный) """
+    name = Column(String(length=60), nullable=False)
+
+
+""" Статус состояния объектов недвижимости """
+class HouseStateStatus(Base):
+   
+    __tablename__ = 'FIAC_HouseStateStatus'
+
+    """ Идентификатор статуса """
+    housestid = Column(Integer, primary_key=True)
+   
+    """ Наименование """
+    name = Column(String(length=60), nullable=False)
+
+
+""" Признак владения """
+class EstateStatus(Base):
+  
+    __tablename__ = 'FIAC_EstateStatus'
+ 
+    """ Идентификатор статуса """ 
+    eststatid = Column(Integer, primary_key=True)
+
+    """ Наименовение """
+    name = Column(String(length=20), nullable=False)
+
+    """ Краткое наименование """
+    shortname = Column(String(length=20))
+
+
+
+""" Признак строения """
+class StructureStatus(Base):
+
+    __tablename__ = 'FIAC_StructureStatus'
+
+    """ Идентификатор """
+    strstatid = Column(Integer, primary_key=True)
+
+    """ Наименовение """
+    name = Column(String(length=20), nullable=False)
+
+    """ Краткое наименование """
+    shortname = Column(String(length=20))
+
+    
+
