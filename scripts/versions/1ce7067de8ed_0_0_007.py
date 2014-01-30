52a346321b7925e61fbb6d464538c35e168db7f9
Stas Merzliakov <merzliakov@gmail.com>
2014-01-30 13:04:34 +0400

    Переименовал поле contactinfo в placeinfo.

---
+++ scripts/versions/1ce7067de8ed_0_0_007.py
@@ -0,0 +1,28 @@
+#-*- coding: UTF-8 -*-
+"""0.0.007
+
+Revision ID: 1ce7067de8ed
+Revises: 2d5ad06e90aa
+Create Date: 2014-01-30 12:29:03.018063
+
+Переименовывание поля contactinfo в placeinfo
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1ce7067de8ed'
+down_revision = '2d5ad06e90aa'
+
+from sqlalchemy.sql import table
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.alter_column('ORG_Organization','contactinfo',new_column_name='placeinfo')
+
+
+def downgrade():
+    op.alter_column('ORG_Organization','placeinfo',new_column_name='contactinfo')
+    
